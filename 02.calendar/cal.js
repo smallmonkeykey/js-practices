8cc0e0a12b50e42a19ec61fcdb702dc22df8bcd8
smallmonkeykey <sarinitakuoo@gmail.com>
2024-04-23 08:48:32 +0900

    キャメルケースに変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,17 +7,17 @@ const now = new Date();
 const year = argv.y ?? now.getFullYear();
 const month = argv.m ?? now.getMonth() + 1;
 
-const first_of_month = new Date(year, month - 1, 1, 23, 59, 59, 999);
-const day_of_week = first_of_month.getDay();
-const end_of_month = new Date(year, month, 0);
-const total_day = end_of_month.getDate();
+const firstOfMonth = new Date(year, month - 1, 1, 23, 59, 59, 999);
+const dayOfWeek = firstOfMonth.getDay();
+const endOfMonth = new Date(year, month, 0);
+const totalDay = endOfMonth.getDate();
 
 console.log(`      ${month}月 ${year}`);
 console.log(`日 月 火 水 木 金 土`);
 
 let day = 1;
-for (let i = 0; i < total_day + day_of_week; i++) {
-  if (i < day_of_week) {
+for (let i = 0; i < totalDay + dayOfWeek; i++) {
+  if (i < dayOfWeek) {
     process.stdout.write("   ");
   } else {
     if (i % 7 === 0) {
