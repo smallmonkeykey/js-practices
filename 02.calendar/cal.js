848730a037dfd59a8ffe6df4776e2b418b9b19ee
smallmonkeykey <sarinitakuoo@gmail.com>
2024-04-23 14:51:19 +0900

    ループカウンタ (i) 以外のループカウンタ (day) があり、見にくいため変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -12,22 +12,22 @@ const dayOfWeek = firstOfMonth.getDay();
 const endOfMonth = new Date(year, month, 0);
 const totalDay = endOfMonth.getDate();
 
+const blank = "   "
+
 console.log(`      ${month}月 ${year}`);
 console.log(`日 月 火 水 木 金 土`);
 
-let day = 1;
-for (let i = 0; i < totalDay + dayOfWeek; i++) {
-  if (i < dayOfWeek) {
-    process.stdout.write("   ");
-  } else {
-    if (i % 7 === 0) {
-      process.stdout.write("\n");
-    }
-    process.stdout.write(String(day).padStart(2, " "));
-    process.stdout.write(" ");
+for (let i=0; i<dayOfWeek; i++) {
+    process.stdout.write(blank)
+}
 
-    day += 1;
-  }
+for (let i=1; i<totalDay+1; i++) {
+  process.stdout.write(String(i).padStart(2));
+    if ((i + dayOfWeek) %7===0) {
+    process.stdout.write("\n");
+  } else {
+    process.stdout.write(" ");
+    }
 }
 
 console.log();
