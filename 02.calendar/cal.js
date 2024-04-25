81dc8546c0e0095569de45a08e826f203365a25e
smallmonkeykey <sarinitakuoo@gmail.com>
2024-04-25 12:23:18 +0900

    不要な変数を削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -9,23 +9,19 @@ const now = new Date();
 const year = argv.y ?? now.getFullYear();
 const month = argv.m ?? now.getMonth() + 1;
 
-const firstOfMonth = new Date(year, month - 1, 1);
-const dayOfWeek = firstOfMonth.getDay();
-const endOfMonth = new Date(year, month, 0);
-const totalDay = endOfMonth.getDate();
-
-const blank = "   ";
+const firstOfMonth = new Date(year, month - 1, 1).getDay();
+const endOfMonth = new Date(year, month, 0).getDate();
 
 console.log(`      ${month}月 ${year}`);
 console.log(`日 月 火 水 木 金 土`);
 
-for (let i = 0; i < dayOfWeek; i++) {
-  process.stdout.write(blank);
+for (let i = 0; i < firstOfMonth; i++) {
+  process.stdout.write("   ");
 }
 
-for (let i = 1; i < totalDay + 1; i++) {
+for (let i = 1; i < endOfMonth + 1; i++) {
   process.stdout.write(String(i).padStart(2));
-  if ((i + dayOfWeek) % 7 === 0) {
+  if ((i + firstOfMonth) % 7 === 0) {
     process.stdout.write("\n");
   } else {
     process.stdout.write(" ");
