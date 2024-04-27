9606b0f277a22d39fc40eb43d91548549dd4d3cf
smallmonkeykey <sarinitakuoo@gmail.com>
2024-04-27 13:26:58 +0900

    変数名を見た時にデータが何が入ってるかわからないので変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -9,21 +9,21 @@ const now = new Date();
 const year = argv.y ?? now.getFullYear();
 const month = argv.m ?? now.getMonth() + 1;
 
-const firstOfMonth = new Date(year, month - 1, 1).getDay();
-const endOfMonth = new Date(year, month, 0).getDate();
+const firstOfMonth = new Date(year, month - 1, 1);
+const endOfMonth = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}`);
 console.log(`日 月 火 水 木 金 土`);
 
-for (let i = 0; i < firstOfMonth; i++) {
+for (let i = 0; i < firstOfMonth.getDay(); i++) {
   process.stdout.write("   ");
 }
 
-for (let i = 1; i < endOfMonth + 1; i++) {
+for (let i = 1; i < endOfMonth.getDate() + 1; i++) {
   process.stdout.write(String(i).padStart(2));
-  if ((i + firstOfMonth) % 7 === 0) {
+  if ((i + firstOfMonth.getDay()) % 7 === 0) {
     process.stdout.write("\n");
-  } else if (i === endOfMonth) {
+  } else if (i === endOfMonth.getDate()) {
     break;
   } else {
     process.stdout.write(" ");
