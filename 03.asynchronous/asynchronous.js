34639d2a9e5b8688738379b25d501dc8ee22f177
smallmonkeykey <sarinitakuoo@gmail.com>
2024-05-21 15:01:35 +0900

    コールバック形式 テーブル作成からレコードの表示まで

---
+++ 03.asynchronous/asynchronous.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3';
+const db = new sqlite3.Database(':memory:');
+
+const createTableQuery = `CREATE TABLE books(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR NOT NULL UNIQUE
+)`;
+
+db.run(createTableQuery, function (err) {
+    if (err) console.error(err.message);
+    console.log("あいう")
+
+    db.run('INSERT INTO books (id, title) VALUES (?, ?)',['1','吾輩は猫である'], function (err) {
+        if (err) console.error(err.message);
+        console.log(`id: ${this.lastID}`)
+    
+        db.all('SELECT * FROM books', function (err, rows) {
+            if (err) {
+                console.error(err.message)
+            } else {
+                rows.forEach((row) => {
+                console.log(`id: ${row.id}, title:${row.title}`);
+                });
+            }
+        });
+    });
+    
+});
+
+db.close();
+
