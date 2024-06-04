f7431d1d1ad6bf01d2961c6bee1ac61220adf030
smallmonkeykey <sarinitakuoo@gmail.com>
2024-06-04 10:19:14 +0900

    関数を作って書き換え

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,52 +1,61 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
+function runAsync(sql, values) {
+    return new Promise((resolve, reject) => {
+        db.run(sql, values, function (err) {
+            if (err) {
+                reject(err.message);
+            } else {
+                resolve(this.lastID);
+            }
+        })
+    })
+}
+
+function allAsync(sql) {
+    return new Promise((resolve, reject) => {
+        db.all(sql, (err, rows) => {
+            if (err) {
+                reject(err.message);
+            } else {
+                resolve(rows);
+            }
+        })
+    })
+}
+
+function closeAsync(sql) {
+	return new Promise((resolve, reject) => {
+		db.close((err) => {
+			if (err) {
+				reject(err.message);
+			} else {
+				resolve();
+			}
+		})
+	})
+}
+
 const createTableQuery = `CREATE TABLE books(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL UNIQUE
 )`;
 
-const createTable = new Promise((resolve) => {
-  db.run(createTableQuery, () => {
-    resolve();
-  });
-});
-
-createTable
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run(
-        "INSERT INTO books (title) VALUES (?)",
-        ["吾輩は猫である"],
-        function () {
-          resolve(this.lastID);
-        },
-      );
-    });
-  })
-  .then((id) => {
-    console.log(`id: ${id}`);
-    return new Promise((resolve) => {
-      db.all("SELECT id, title FROM books", (err, rows) => {
-        resolve(rows);
-      });
-    });
-  })
-  .then((rows) => {
-    rows.forEach((row) => {
-      console.log(`id: ${row.id}, title:${row.title}`);
-    });
-    return new Promise((resolve) => {
-      db.run("DROP TABLE books", () => {
-        resolve("テーブルを削除しました");
-      });
-    });
-  })
-  .then((message) => {
-    console.log(message);
-    new Promise((resolve) => {
-      db.close(() => {
-        resolve;
-      });
-    });
-  });
+runAsync(createTableQuery)
+    .then(() => {
+      return runAsync("INSERT INTO books (title) VALUES (?)",["吾輩は猫である"])
+    })
+    .then((id) => {
+      console.log(id)
+      return allAsync("SELECT id, title FROM books")
+    })
+    .then((rows) => {
+      rows.forEach((row) => {
+				console.log(`id: ${row.id}, title:${row.title}`);
+			});
+			return runAsync("DROP TABLE books")
+		})
+	.then(() => {
+		return closeAsync()
+	})
