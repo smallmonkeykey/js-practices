30d81624e6de78ea637bdd77bfe72f48598b33dc
smallmonkeykey <sarinitakuoo@gmail.com>
2024-06-05 16:07:23 +0900

    async/awaitエラーなしを作成

---
+++ 03.asynchronous/async_await.js
@@ -0,0 +1,35 @@
+import { createDatabase, runAsync, allAsync, closeAsync } from "./function.js";
+
+const createTableQuery = `CREATE TABLE books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+)`;
+
+async function operateSqlite3() {
+  const db=createDatabase();
+
+  const createTableQuery = `CREATE TABLE books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+   )`;
+  
+  try {
+    await runAsync(db, createTableQuery);
+
+    const lastId = await runAsync(db, "INSERT INTO books (title) VALUES (?)", ["吾輩は猫である",]);
+    console.log(lastId);
+
+    const rows = await allAsync(db, "SELECT id, title FROM books");
+    rows.forEach((row) => {
+      console.log(`id: ${row.id}, title:${row.title}`);
+    });
+
+    await runAsync(db, "DROP TABLE books");
+    await closeAsync(db);
+
+  } catch {
+    
+  }
+}
+
+operateSqlite3();
