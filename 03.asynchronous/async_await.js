df34748561a000f000ffc879a056d8bd23125e24
smallmonkeykey <sarinitakuoo@gmail.com>
2024-06-05 16:33:40 +0900

    finallyを追加とlint修正

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -1,29 +1,28 @@
 import { createDatabase, runAsync, allAsync, closeAsync } from "./function.js";
 
 async function operateSqlite3() {
-  const db=createDatabase();
+  const db = createDatabase();
 
   const createTableQuery = `CREATE TABLE books(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL UNIQUE
    )`;
-  
+
   try {
     await runAsync(db, createTableQuery);
 
-    const lastId = await runAsync(db, "INSERT INTO books (title) VALUES (?)", ["吾輩は猫である",]);
+    const lastId = await runAsync(db, "INSERT INTO books (title) VALUES (?)", [
+      "吾輩は猫である",
+    ]);
     console.log(lastId);
 
     const rows = await allAsync(db, "SELECT id, title FROM books");
     rows.forEach((row) => {
       console.log(`id: ${row.id}, title:${row.title}`);
     });
-
+  } finally {
     await runAsync(db, "DROP TABLE books");
     await closeAsync(db);
-
-  } catch {
-    
   }
 }
 
--- 03.asynchronous/async_await_error.js
+++ 03.asynchronous/async_await_error.js
@@ -1,41 +1,41 @@
 import { createDatabase, runAsync, allAsync, closeAsync } from "./function.js";
 
 async function operateSqlite3() {
-  const db=createDatabase();
+  const db = createDatabase();
 
   const createTableQuery = `CREATE TABLE books(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL UNIQUE
    )`;
-  
+
   try {
     await runAsync(db, createTableQuery);
 
-      try {
-          await runAsync(db, "INSERT INTO book (title) VALUES (?)", ["吾輩は猫である",]);
-          console.log(lastId);
-      } catch (err) {
-          console.log(err.message)
-      }
+    try {
+      const lastId = await runAsync(db, "INSERT INTO book (title) VALUES (?)", [
+        "吾輩は猫である",
+      ]);
+      console.log(lastId);
+    } catch (err) {
+      console.log(err.message);
+    }
 
-      try {
-           const rows = await allAsync(db, "SELECT id, title FROM boo");
-           rows.forEach((row) => {
-           console.log(`id: ${row.id}, title:${row.title}`);
-           });
-      } catch (err) {
-          console.log(err.message)
-      }
+    try {
+      const rows = await allAsync(db, "SELECT id, title FROM boo");
+      rows.forEach((row) => {
+        console.log(`id: ${row.id}, title:${row.title}`);
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
 
     const rows = await allAsync(db, "SELECT id, title FROM books");
     rows.forEach((row) => {
       console.log(`id: ${row.id}, title:${row.title}`);
     });
-
+  } finally {
     await runAsync(db, "DROP TABLE books");
     await closeAsync(db);
-
-  } catch {
   }
 }
 
