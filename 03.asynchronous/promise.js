92b2e95dc0a0704308c39aab6abf3dbe63c7eb05
smallmonkeykey <sarinitakuoo@gmail.com>
2024-06-06 19:05:59 +0900

    アロー関数の中身が return しかないときはブロック文体ではなく簡潔文体を使う

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -4,11 +4,9 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 runAsync(db, "CREATE TABLE books(id INTEGER AUTO_INCREMENT PRIMARY KEY,title VARCHAR NOT NULL UNIQUE)")
-  .then(() => {
-    return runAsync(db, "INSERT INTO books(title) VALUES(?)", [
+  .then(() => runAsync(db, "INSERT INTO books(title) VALUES(?)", [
       "吾輩は猫である",
-    ]);
-  })
+    ]))
   .then((result) => {
     console.log(`id: ${result.lastID}`);
     return allAsync(db, "SELECT id, title FROM books");
