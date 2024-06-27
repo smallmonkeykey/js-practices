28c756bef7d4a78d8755bb716960b8ec6a75ef95
smallmonkeykey <sarinitakuoo@gmail.com>
2024-06-27 13:56:45 +0900

    空行をいれてスタイルを統一

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -9,16 +9,16 @@ import { runAsync, allAsync, closeAsync } from "./db_async_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-(async () => {
-  await runAsync(db, createTableQuery);
+await runAsync(db, createTableQuery);
 
-  const result = await runAsync(db, insertBookQuery, ["吾輩は猫である"]);
-  console.log(`id: ${result.lastID}`);
+const result = await runAsync(db, insertBookQuery, ["吾輩は猫である"]);
+console.log(`id: ${result.lastID}`);
 
-  const rows = await allAsync(db, selectBookQuery);
-  rows.forEach((row) => {
-    console.log(`id: ${row.id}, title: ${row.title}`);
-  });
-  await runAsync(db, dropTableQuery);
-  await closeAsync(db);
-})();
+const rows = await allAsync(db, selectBookQuery);
+rows.forEach((row) => {
+  console.log(`id: ${row.id}, title: ${row.title}`);
+});
+
+await runAsync(db, dropTableQuery);
+
+await closeAsync(db);
--- 03.asynchronous/async_await_error.js
+++ 03.asynchronous/async_await_error.js
@@ -9,29 +9,28 @@ import { runAsync, allAsync, closeAsync } from "./db_async_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-(async () => {
-  await runAsync(db, createTableQuery);
+await runAsync(db, createTableQuery);
 
-  try {
-    await runAsync(db, insertBookErrorQuery, ["吾輩は猫である"]);
-  } catch (err) {
-    if (err.code === "SQLITE_ERROR") {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
+try {
+  await runAsync(db, insertBookErrorQuery, ["吾輩は猫である"]);
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(err.message);
+  } else {
+    throw err;
   }
+}
 
-  try {
-    await allAsync(db, selectBookErrorQuery);
-  } catch (err) {
-    if (err.code === "SQLITE_ERROR") {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
+try {
+  await allAsync(db, selectBookErrorQuery);
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(err.message);
+  } else {
+    throw err;
   }
+}
 
-  await runAsync(db, dropTableQuery);
-  await closeAsync(db);
-})();
+await runAsync(db, dropTableQuery);
+
+await closeAsync(db);
